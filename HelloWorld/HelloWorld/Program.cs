61a01c33b05d548295483b8561d15fc6f4152796
LR <1204326279@qq.com>
2017-06-30 14:37:26 +0800

    C#代码

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿// 引用的系统命名空间
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 创建的项目的命名空间
+namespace HelloWorld
+{
+    // 默认创建的一个类
+    class Program
+    {
+        // main函数是整个应用程序的入口
+        static void Main(string[] args)
+        {
+            // 用来向控制台输出信息
+            Console.WriteLine("你好，VR");
+            // 等待用户输入任意字符
+            Console.ReadKey();
+        }
+    }
+}
